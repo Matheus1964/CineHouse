58172fd7700735e87bba3816349074e3de30ddc3
Matheus1964 <mateusaragao1@hotmail.com>
2022-06-08 02:26:43 -0300

    correções

--- js/api.js
+++ js/api.js
@@ -69,7 +69,7 @@ function displayMovieDetails(details) {
     position: absolute;
     top: 8.5rem;
     width: 100%;
-    height: 262%;
+    height: 150%;
     background-color: rgba(0, 27, 40, 1);
     z-index: 2;
   }</style>
