a1319a0faab99e75d79ae079fd4203c630bb5532
Matheus1964 <mateusaragao1@hotmail.com>
2022-06-09 13:31:59 -0300

    Fiz algumas correçoes

--- js/redes.js
+++ js/redes.js
@@ -1,10 +1,10 @@
 const tela = document.getElementById('redes')
-const fundo = document.getElementById('matheus')
+const fundo = document.getElementById('predes')
 tela.addEventListener('click', function () {
   console.log('deu certo')
   fundo.innerHTML = `
   <style>
-  #matheus {
+  #predes {
     position: absolute;
     top: 8.5rem;
     width: 100%;
@@ -14,7 +14,7 @@ tela.addEventListener('click', function () {
   }</style>
   <h1 id="redes1">Redes Social CineHouse</h1>
   <div class="linha"><div class="card" style="width: 18rem;">
-  <img style=" height: 10rem;"src="img/foto/facebook.png" class="card-img-top" alt="...">
+  <img style=" height: 10rem;"src="../img/facebook.png" class="card-img-top" alt="...">
   <div class="card-body">
     <h5 class="card-title">Facebook</h5>
     <p class="card-text">Essa é a rede social cinehouse</p>
@@ -22,7 +22,7 @@ tela.addEventListener('click', function () {
   </div>
 </div>
 <div class="card" style="width: 18rem;">
-  <img style=" height: 10rem;" src="img/foto/twitter.png" class="card-img-top" alt="...">
+  <img style=" height: 10rem;" src="../img/twitter.png" class="card-img-top" alt="...">
   <div class="card-body">
     <h5 class="card-title">Twitter</h5>
     <p class="card-text">matheus</p>
@@ -30,7 +30,7 @@ tela.addEventListener('click', function () {
   </div>
 </div>
 <div class="card" style="width: 18rem;">
-  <img style=" height: 10rem;"src="img/foto/instagram.jpg" class="card-img-top" alt="...">
+  <img style=" height: 10rem;"src="../img/instagram.jpg" class="card-img-top" alt="...">
   <div class="card-body">
     <h5 class="card-title">Instagram</h5>
     <p class="card-text">matheus</p>
@@ -41,7 +41,7 @@ tela.addEventListener('click', function () {
 </div>
 <div class="linha2">
 <div class="card" style="width: 18rem;">
-  <img src="img/foto/github.png" class="card-img-top" alt="...">
+  <img src="../img/github.png" class="card-img-top" alt="...">
   <div class="card-body">
     <h5 class="card-title">Card title</h5>
     <p class="card-text">matheus</p>
