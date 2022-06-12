8ed65e54f9d243971892cfd93c0727d51ec5e34d
Matheus1964 <mateusaragao1@hotmail.com>
2022-06-11 22:00:24 -0300

    alteraçoes

--- js/redes.js
+++ js/redes.js
@@ -13,7 +13,8 @@ tela.addEventListener('click', function () {
     z-index: 2;
   }</style>
   <h1 id="redes1">Redes Social CineHouse</h1>
-  <div class="linha"><div class="card" style="width: 18rem;">
+  <div class="linha">
+  <div class="card" style="width: 18rem;">
   <img style=" height: 10rem;"src="../img/facebook.png" class="card-img-top" alt="...">
   <div class="card-body">
     <h5 class="card-title">Facebook</h5>
@@ -37,9 +38,6 @@ tela.addEventListener('click', function () {
     <a href="https://www.instagram.com/" class="btn btn-primary">Acessar</a>
   </div>
 </div>
-<h1 id="redes2">Redes Sociais: Matheus</h1>
-</div>
-<div class="linha2">
 <div class="card" style="width: 18rem;">
   <img src="../img/github.png" class="card-img-top" alt="...">
   <div class="card-body">
@@ -48,13 +46,6 @@ tela.addEventListener('click', function () {
     <a href="https://github.com/Matheus1964" class="btn btn-primary">Acessar</a>
   </div>
 </div>
-<div class="card" style="width: 18rem;">
-  <img src="..." class="card-img-top" alt="...">
-  <div class="card-body">
-    <h5 class="card-title">Card title</h5>
-    <p class="card-text">matheus</p>
-    <a href="#" class="btn btn-primary">Acessar</a>
-  </div>
 </div>
-</div>`
+`
 })
