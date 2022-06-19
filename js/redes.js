d13968c0b5dff6212bd8aa554a2771c02f8dcc31
Taiyo <103234811+taiyotk@users.noreply.github.com>
2022-06-18 23:06:37 -0300

    Correção feita
    A altura do id #predes foi aumentada para cobrir mais a tela

--- js/redes.js
+++ js/redes.js
@@ -8,7 +8,7 @@ tela.addEventListener('click', function () {
     position: absolute;
     top: 8.5rem;
     width: 100%;
-    height: 150%;
+    height: 180%;
     background-color: rgba(0, 27, 40, 1);
     z-index: 2;
   }</style>
